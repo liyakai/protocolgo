a1ed8ac5ad595d50a87ecb36cae2eec785bfbc27
liyakai <liyakai@mail.ustc.edu.cn>
2024-01-19 21:47:26 +0800

    初始化GUI

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+)
 
 func main() {
 	fmt.Println("hello world.")
+	a := app.New()
+	w := a.NewWindow("Hello world")
+	hello := widget.NewLabel("Hello Fyne!")
+	w.SetContent(container.NewVBox(
+		hello,
+		widget.NewButton("Hi!", func() {
+			hello.SetText("Welcome :)")
+		}),
+	))
+	w.ShowAndRun()
 }
