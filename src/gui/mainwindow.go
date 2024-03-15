b1fffbd5c50fa92bc53d04d7c68a5c1b9f5f2246
liyakai <liyakai@mail.ustc.edu.cn>
2024-03-15 15:16:14 +0800

    完成 创建/打开/保存 xml逻辑

--- src/gui/mainwindow.go
+++ src/gui/mainwindow.go
@@ -34,17 +34,25 @@ func (stapp *StApp) CreateMenuItem() {
 	// 创建新文件
 	newMenuItem := fyne.NewMenuItem("new..", func() {
 		// 打开文件
-		file_picker := dialog.NewFileOpen(func(reader fyne.URIReadCloser, err error) {
-			// Check for errors
+		saveDialog := dialog.NewFileSave(func(writer fyne.URIWriteCloser, err error) {
 			if err != nil {
-				log.Println("Failed to open file:", err)
+				logrus.Error("Failed to NewFileSave:", err)
+				dialog.ShowError(err, *stapp.Window)
 				return
 			}
+			if writer == nil { // user cancelled
+				logrus.Info("Failed to NewFileSave, user cancelled.")
+				return
+			}
+			xml_file_path := writer.URI().Path()
+			// 设定 当前打开的xml文件路径
+			stapp.CoreMgr.SetCurrXmlFilePath(xml_file_path)
+			stapp.CoreMgr.CreateNewXml()
+			// 在此处写入你的文件
 		}, *stapp.Window)
-		file_picker.Resize(fyne.NewSize(700, 500))
-		file_picker.SetFilter(storage.NewExtensionFileFilter([]string{".txt", ".xml"}))
-		// 显示打开文件的UI
-		file_picker.Show()
+
+		saveDialog.SetFileName("protocolgo.xml")
+		saveDialog.Show()
 
 	})
 	// 打开菜单项
@@ -53,11 +61,15 @@ func (stapp *StApp) CreateMenuItem() {
 		file_picker := dialog.NewFileOpen(func(reader fyne.URIReadCloser, err error) {
 			// Check for errors
 			if err != nil {
-				log.Println("Failed to open file:", err)
+				log.Println("Failed to NewFileOpen:", err)
 				return
 			}
 			xml_file_path := reader.URI().Path()
-			logrus.WithFields(logrus.Fields{"file path": xml_file_path}).Info("Open xml file.")
+			// 设定 当前打开的xml文件路径
+			stapp.CoreMgr.SetCurrXmlFilePath(xml_file_path)
+			// 读取到内存
+			stapp.CoreMgr.ReadXmlFromFile(reader)
+			logrus.Info("Open xml file done.file path:", xml_file_path)
 		}, *stapp.Window)
 		file_picker.Resize(fyne.NewSize(700, 500))
 		file_picker.SetFilter(storage.NewExtensionFileFilter([]string{".txt", ".xml"}))
@@ -65,8 +77,24 @@ func (stapp *StApp) CreateMenuItem() {
 		file_picker.Show()
 
 	})
+	// 保存菜单项
+	saveMenuItem := fyne.NewMenuItem("save..", func() {
+		dialog.ShowConfirm("Confirmation", "Are you sure you want to Save?",
+			func(response bool) {
+				if response {
+					// addition logic to save file goes here
+					if stapp.CoreMgr.SaveToXmlFile() {
+						dialog.ShowInformation("Saved", "File successfully saved.", *stapp.Window)
+					} else {
+						dialog.ShowInformation("Error", "Unexist opened xml or invalid xml path.", *stapp.Window)
+					}
+
+				}
+			}, *stapp.Window)
+
+	})
 	// 创建一个一级菜单
-	fileMenu := fyne.NewMenu("File", newMenuItem, openMenuItem)
+	fileMenu := fyne.NewMenu("File", newMenuItem, openMenuItem, saveMenuItem)
 	// 创建菜单栏
 	menu := fyne.NewMainMenu(fileMenu)
 
--- src/logic/coremanager.go
+++ src/logic/coremanager.go
@@ -1,7 +1,10 @@
 package logic
 
 import (
+	"io"
+
 	"github.com/beevik/etree"
+	"github.com/sirupsen/logrus"
 )
 
 type CoreManager struct {
@@ -12,29 +15,50 @@ type CoreManager struct {
 func (Stapp *CoreManager) CreateNewXml() {
 	// 如果现在打开的xml不为空,则先保存现在打开的xml
 	if nil != Stapp.DocEtree {
-		Stapp.SaveToFile()
+		Stapp.SaveToXmlFile()
 	}
 	// 创建新的xml
-	DocEtree := etree.NewDocument()
-	DocEtree.CreateProcInst("xml", `version="1.0" encoding="UTF-8"`)
-	DocEtree.CreateProcInst("xml-stylesheet", `type="text/xsl" href="style.xsl"`)
-
+	Stapp.DocEtree = etree.NewDocument()
+	Stapp.DocEtree.CreateProcInst("xml", `version="1.0" encoding="UTF-8"`)
+	Stapp.DocEtree.CreateProcInst("xml-stylesheet", `type="text/xsl" href="style.xsl"`)
+	Stapp.SaveToXmlFile()
+	logrus.Info("CreateNewXml done.")
 }
 
-func (Stapp *CoreManager) ReadXmlFromFile() {
+func (Stapp *CoreManager) ReadXmlFromFile(reader io.Reader) {
 	// 如果现在打开的xml不为空,则先保存现在打开的xml
 	if nil != Stapp.DocEtree {
-		Stapp.SaveToFile()
+		Stapp.SaveToXmlFile()
 	}
 	doc := etree.NewDocument()
-	if err := doc.ReadFromFile("bookstore.xml"); err != nil {
+	if _, err := doc.ReadFrom(reader); err != nil {
 		panic(err)
 	}
+	logrus.Info("ReadXmlFromFile done.")
 }
 
-func (Stapp *CoreManager) SaveToFile() {
+func (Stapp *CoreManager) SaveToXmlFile() bool {
+	if nil == Stapp.DocEtree || Stapp.XmlFilePath == "" {
+		logrus.Warn("SaveToXmlFile failed. invalid param. Stapp.XmlFilePath:", Stapp.XmlFilePath)
+		return false
+	}
+	Stapp.DocEtree.WriteToFile(Stapp.XmlFilePath)
+	logrus.Info("SaveToXmlFile done. XmlFilePath:", Stapp.XmlFilePath)
+	return true
+}
+
+func (Stapp *CoreManager) CloseCurrXmlFile() {
 	if nil == Stapp.DocEtree {
+		logrus.Warn("CloseCurrXmlFile failed. Stapp.DocEtree is nil.")
 		return
 	}
-	Stapp.SaveToFile()
+	Stapp.SaveToXmlFile()
+	Stapp.XmlFilePath = ""
+	logrus.Info("CloseCurrXmlFile done.")
+}
+
+func (Stapp *CoreManager) SetCurrXmlFilePath(currFilePath string) {
+	Stapp.CloseCurrXmlFile()
+	Stapp.XmlFilePath = currFilePath
+	logrus.Info("SetCurrXmlFilePath done.currFilePath:", currFilePath)
 }
